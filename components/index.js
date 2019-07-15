cfeef724a94d8afeb68acd9b00b7016a0370e602
cchungmin <min427@gmail.com>
2019-07-15 19:18:50 +0900

    Basic component

--- components/index.js
+++ components/index.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default () => (
+const Index = ({ data }) => (
   <React.Fragment>
-    Welcome
+    <ul>
+      {
+        data.map(el => (
+          <li key={el.dt}>
+            {el.weather[0].description}
+          </li>
+        ))
+      }
+    </ul>
   </React.Fragment>
 );
+
+Index.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.objectOf(
+      PropTypes.oneOfType(
+        PropTypes.string,
+        PropTypes.number,
+        PropTypes.arrayOf(
+          PropTypes.string,
+          PropTypes.number,
+        ),
+        PropTypes.objectOf(
+          PropTypes.string,
+          PropTypes.number,
+        ),
+      ),
+    ),
+  ),
+};
+
+Index.defaultProps = {
+  data: [],
+};
+
+export default Index;
