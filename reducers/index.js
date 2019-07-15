ee5122749b6d387e7d0127a80bbbffbb668dedb7
cchungmin <min427@gmail.com>
2019-07-15 18:33:46 +0900

    Basic reducer

--- reducers/index.js
+++ reducers/index.js
@@ -1,1 +1,19 @@
-export default () => {};
+import types from '../actions/types';
+
+export const INITIAL_STATE = {
+  forecastData: [],
+};
+
+const reducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case types.FETCH_WEATHER_SUCCESS:
+      return {
+        ...state,
+        forecastData: action.data,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
