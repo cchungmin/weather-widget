f844eba4a4c47d22fd2bd8bd62334924564cd057
Chung-Min Cheng <min427@gmail.com>
2019-07-16 22:11:36 +0900

    Uses metric temperature

--- actions/weather.js
+++ actions/weather.js
@@ -6,9 +6,9 @@ const APP_ID = '81a26fa8a34968e47fc164ca9ba3f439';
 
 export const fetchWeather = (lat, lon) => async (dispatch) => {
   dispatch({ type: types.FETCH_WEATHER });
-  const data = await get(`${API_PATH}lat=${lat}&lon=${lon}&appid=${APP_ID}`);
+  const data = await get(`${API_PATH}lat=${lat}&lon=${lon}&appid=${APP_ID}&units=metric`);
   if (data.cod === '200') {
-    dispatch({ data: data.list, type: types.FETCH_WEATHER_SUCCESS });
+    dispatch({ data, type: types.FETCH_WEATHER_SUCCESS });
   } else {
     dispatch({ type: types.FETCH_WEATHER_FAILED });
   }
