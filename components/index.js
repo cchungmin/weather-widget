a73e1b456d0328de843b53d7a6d2f5db0770ab7b
cchungmin <min427@gmail.com>
2019-07-16 23:40:07 +0900

    Datetime with some styles

--- components/index.js
+++ components/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import css from '../styles.scss';
+import { getWeekday, getDateTimeStr } from '../utils/datetime';
+
 
 const Index = ({ data }) => (
   <main className={css['main-container']}>
@@ -9,8 +11,18 @@ const Index = ({ data }) => (
       {
         data.list && data.list.map(el => (
           <li key={el.dt} className={css.widget}>
-            <h2>{new Date(el.dt_txt).getDay()}</h2>
-            <div>{el.weather[0].description}</div>
+            <h2>{getWeekday(el.dt_txt)}</h2>
+            <div>{getDateTimeStr(el.dt_txt)}</div>
+            <p className={css['widget-desc']}>{el.weather[0].description}</p>
+            <div className={css['widget-temp']}>
+              <span>{el.main.temp}</span>
+              <span>&#8451;</span>
+            </div>
+            <div className={css['widget-country']}>
+              <span>{data.city.name}</span>
+              ,&nbsp;
+              <span>{data.city.country}</span>
+            </div>
           </li>
         ))
       }
---
+++ utils/datetime.js
@@ -0,0 +1,16 @@
+const getWeekday = date => new Intl.DateTimeFormat(['en-US'], {
+  weekday: 'long',
+}).format(new Date(date));
+
+const getDateTimeStr = date => new Intl.DateTimeFormat(['en-US'], {
+  day: 'numeric',
+  month: 'short',
+  year: 'numeric',
+  hour: '2-digit',
+  minute: '2-digit',
+}).format(new Date(date));
+
+export {
+  getWeekday,
+  getDateTimeStr,
+};
