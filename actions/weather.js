6d8c349df355ed0eda29e67ce9011109946bf7b7
Chung-Min Cheng <min427@gmail.com>
2019-07-16 13:53:44 +0900

    Dispatch actions and types

---
+++ actions/weather.js
@@ -0,0 +1,19 @@
+import types from './types';
+import { get } from '../utils/request';
+
+const API_PATH = 'https://api.openweathermap.org/data/2.5/forecast?';
+const APP_ID = '81a26fa8a34968e47fc164ca9ba3f439';
+
+export const fetchWeather = (lat, lon) => async (dispatch) => {
+  dispatch({ type: types.FETCH_WEATHER });
+  const data = await get(`${API_PATH}lat=${lat}&lon=${lon}&appid=${APP_ID}`);
+  if (data.cod === '200') {
+    dispatch({ data: data.list, type: types.FETCH_WEATHER_SUCCESS });
+  } else {
+    dispatch({ type: types.FETCH_WEATHER_FAILED });
+  }
+};
+
+export default {
+  fetchWeather,
+};
